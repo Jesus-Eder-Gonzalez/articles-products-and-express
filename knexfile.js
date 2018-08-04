c5ef0f9d924da28163107cc1a9573c856dfe4f15
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-08-03 16:44:19 -1000

    Added knex installation and will setup db

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "articles_products",
+      user: "articles_products_user",
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    },
+    debug: true  
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
